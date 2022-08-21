8f9a8e240dc6adc0b9dc759544dcf0a359de459e
Robert Czyzewski <rob.czyzewski@outlook.com>
2022-08-21 19:18:48 -0400

    Adding counter

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
+let counter = 0;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
+    counter++;
+    console.log('Servicing request ', counter);
     res.end('Hello World');
 });
 
