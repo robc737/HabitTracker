ab38c3d32005a1c0b23212767b2f559681f64937
Robert Czyzewski <robc737@outlook.com>
2023-02-20 13:21:22 -0500

    prettier

--- public/js/habit-tracker.js
+++ public/js/habit-tracker.js
@@ -1,41 +1,38 @@
 const linkMap = {
-    'Weekly' : {viewId: "weekly", url: '/weekly'},
-    'Monthly' : {viewId: 'monthly', url: '/monthly'},
-    'Create Habit' : {viewId: 'create', url: '/create'},
-    'Edit Habit' : {viewId: 'edit', url: '/edit'}
-}
+  Weekly: { viewId: "weekly", url: "/weekly" },
+  Monthly: { viewId: "monthly", url: "/monthly" },
+  "Create Habit": { viewId: "create", url: "/create" },
+  "Edit Habit": { viewId: "edit", url: "/edit" },
+};
 
 let headerLinks = document.querySelectorAll(".header a");
 console.log(headerLinks);
-headerLinks.forEach(headerLink => {
-   headerLink.addEventListener("click", (event) => {
-       const viewObj = linkMap[event.target.innerText];
-       showView(viewObj.viewId);
-       window.history.pushState({}, '', viewObj.url);
-   });
+headerLinks.forEach((headerLink) => {
+  headerLink.addEventListener("click", (event) => {
+    const viewObj = linkMap[event.target.innerText];
+    showView(viewObj.viewId);
+    window.history.pushState({}, "", viewObj.url);
+  });
 });
 
 function showView(viewId) {
-    let views = document.querySelectorAll(".view");
-    views.forEach( view => {
-        view.classList.add("d-none");
-    });
+  let views = document.querySelectorAll(".view");
+  views.forEach((view) => {
+    view.classList.add("d-none");
+  });
 
-    let view = document.getElementById(viewId);
-    view.classList.remove("d-none");
+  let view = document.getElementById(viewId);
+  view.classList.remove("d-none");
 }
 
 window.addEventListener("popstate", showViewByUrl);
+
 function showViewByUrl() {
-    let path = window.location.pathname;
-    showView(path.slice(1));
+  let path = window.location.pathname;
+  showView(path.slice(1));
 }
 
-showView('weekly');
-window.history.pushState({}, '', 'weekly');
+showView("weekly");
+window.history.pushState({}, "", "weekly");
 
-
-
-const showView2 = () => {
-
-}
+const showView2 = () => {};
